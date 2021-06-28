b89812ff8aada6757d15b89ae05b9cff8a66d05d
Juan <pront23@gmail.com>
2021-06-29 01:06:04 +0900

    created Restaurant Screen component

--- App.js
+++ App.js
@@ -1,35 +1,13 @@
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
+
 import React from 'react';
-import { Searchbar } from 'react-native-paper';
-import { StatusBar, StyleSheet, Text, SafeAreaView, View } from 'react-native';
+import { RestaurantsScreen } from './src/features/restaurants/screens/restaurants.screen';
 
 export default function App() {
   return (
     <>
-      <SafeAreaView style={styles.container}>
-        <View style={styles.search}>
-          <Searchbar />
-        </View>
-        <View style={styles.listContainer}>
-          <Text>List</Text>
-        </View>
-      </SafeAreaView>
+      <RestaurantsScreen />
       <ExpoStatusBar style="auto" />
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight,
-  },
-  search: {
-    padding: 16,
-  },
-  listContainer: {
-    flex: 1,
-    padding: 16,
-    backgroundColor: 'blue',
-  },
-});

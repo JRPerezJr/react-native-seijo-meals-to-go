d85ebdfc2d95eb504453c04c59dd6f0157292ffc
Juan <pront23@gmail.com>
2021-06-28 20:01:42 +0900

    setup safe area for android and ios

--- App.js
+++ App.js
@@ -1,9 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
+import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar, StyleSheet, Text, SafeAreaView, View } from 'react-native';
 
 export default function App() {
-  return <View></View>;
+  return (
+    <>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.viewContainer}>
+          <Text>Search</Text>
+        </View>
+        <View style={styles.listContainer}>
+          <Text>List</Text>
+        </View>
+      </SafeAreaView>
+      <ExpoStatusBar style="auto" />
+    </>
+  );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight,
+  },
+  viewContainer: {
+    padding: 16,
+    backgroundColor: 'green',
+  },
+  listContainer: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: 'blue',
+  },
+});

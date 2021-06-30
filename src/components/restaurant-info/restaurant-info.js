2e311841daa8c265faa6a822c6bd84c6846fce89
Juan <pront23@gmail.com>
2021-06-30 18:09:12 +0900

    updated the name and rating info

--- src/components/restaurant-info/restaurant-info.js
+++ src/components/restaurant-info/restaurant-info.js
@@ -4,14 +4,14 @@ import { CardAppComponent } from '../card/card.component';
 
 export const RestaurantInfo = ({ restaurant = {} }) => {
   const {
-    name = 'Some bloody restaurant',
+    name = 'The Tasty Moose',
     icon = 'some icon',
     photos = [
       'https://weneedfun.com/wp-content/uploads/2015/10/Beautiful-Food-Photos-2.jpg',
     ],
     address = '1234 Any St. Any Town, USA',
     isOpenNow = true,
-    rating = '10',
+    rating = '5',
     isClosedTemporarily = true,
   } = restaurant;
   return (

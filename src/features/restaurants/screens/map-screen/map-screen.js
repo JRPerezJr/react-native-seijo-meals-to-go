9bd892f2f58088f918448a4d5a93fb43ed919ba4
Juan <pront23@gmail.com>
2021-07-01 02:00:08 +0900

    added generic components

---
+++ src/features/restaurants/screens/map-screen/map-screen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text } from 'react-native';
+import { StyledSafeAreaView } from '../../../../components/utilities/safe-area.component';
+
+export const MapScreen = () => {
+  return (
+    <>
+      <StyledSafeAreaView>
+        <Text>Hello from MapScreen</Text>
+      </StyledSafeAreaView>
+    </>
+  );
+};
---
+++ src/features/restaurants/screens/settings-screen/settings-screen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text } from 'react-native';
+import { StyledSafeAreaView } from '../../../../components/utilities/safe-area.component';
+
+export const SettingsScreen = () => {
+  return (
+    <>
+      <StyledSafeAreaView>
+        <Text>Hello from Settings</Text>
+      </StyledSafeAreaView>
+    </>
+  );
+};

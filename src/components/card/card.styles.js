93aeed920015e3cbe2c8ef51d0a782e17c9569b6
Juan <pront23@gmail.com>
2021-07-01 00:31:41 +0900

    removed margin-bottom

--- src/components/card/card.styles.js
+++ src/components/card/card.styles.js
@@ -4,7 +4,6 @@ import { Image, View } from 'react-native';
 
 export const StyledCard = styled(Card)`
   background-color: ${props => props.theme.colors.bg.primary};
-  margin-bottom: ${props => props.theme.space[3]};
 `;
 
 export const StyledCardCover = styled(Card.Cover)`

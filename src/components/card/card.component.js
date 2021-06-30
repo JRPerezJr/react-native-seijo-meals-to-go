343d2de8785a0b1e5d59c1b130bf0c97fa303235
Juan <pront23@gmail.com>
2021-06-30 19:02:25 +0900

    added closed temp bed icon

--- src/components/card/card.component.js
+++ src/components/card/card.component.js
@@ -5,18 +5,52 @@ import {
   StyledCardCover,
   StyledCardStyle,
   StyledTitle,
+  StyledText,
   StyledParagraph,
+  Rating,
+  Section,
+  SectionEnd,
+  StyledView,
+  StyledImage,
 } from './card.styles';
 
 import { Star } from '../../../assets/star';
+import { OpenIcon } from '../../../assets/open';
+
 export const CardAppComponent = ({ restaurantData }) => {
-  const { address, name, photos } = restaurantData.props;
+  const {
+    address,
+    icon,
+    isClosedTemporarily,
+    isOpenNow,
+    name,
+    photos,
+    rating,
+  } = restaurantData.props;
+
+  const ratingArray = Array.from(new Array(Math.floor(rating)));
+
   return (
     <StyledCardStyle elevation={5}>
       <StyledCardCover key={name} source={{ uri: photos[0] }} />
       <Card.Content>
         <StyledTitle>{name}</StyledTitle>
-        <Star />
+        <Section>
+          <Rating>
+            {ratingArray.map(() => (
+              <Star />
+            ))}
+          </Rating>
+          <SectionEnd>
+            {isClosedTemporarily && (
+              <StyledText variant="label">CLOSED TEMPORARILY</StyledText>
+            )}
+            <StyledView />
+            {isOpenNow && <OpenIcon />}
+            <StyledView />
+            <StyledImage source={{ uri: icon }} />
+          </SectionEnd>
+        </Section>
         <StyledParagraph>{address}</StyledParagraph>
       </Card.Content>
     </StyledCardStyle>

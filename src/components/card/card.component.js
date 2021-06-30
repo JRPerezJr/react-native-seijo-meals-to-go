7a9270b762e16fd1638cb2b948aa3d3caab8e09e
Juan <pront23@gmail.com>
2021-06-30 17:24:47 +0900

    updated font family and size

--- src/components/card/card.component.js
+++ src/components/card/card.component.js
@@ -1,7 +1,12 @@
 import * as React from 'react';
 // import { StyleSheet } from 'react-native';
 import { Card, Paragraph } from 'react-native-paper';
-import { StyledCardCover, StyledCardStyle, StyledTitle } from './card.styles';
+import {
+  StyledCardCover,
+  StyledCardStyle,
+  StyledTitle,
+  StyledParagraph,
+} from './card.styles';
 
 export const CardAppComponent = ({ restaurantData }) => {
   const { address, name, photos } = restaurantData.props;
@@ -10,7 +15,7 @@ export const CardAppComponent = ({ restaurantData }) => {
       <StyledCardCover key={name} source={{ uri: photos[0] }} />
       <Card.Content>
         <StyledTitle>{name}</StyledTitle>
-        <Paragraph>{address}</Paragraph>
+        <StyledParagraph>{address}</StyledParagraph>
       </Card.Content>
     </StyledCardStyle>
   );
--- src/components/card/card.styles.js
+++ src/components/card/card.styles.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { Card, Title } from 'react-native-paper';
+import { Card, Title, Paragraph } from 'react-native-paper';
 
 export const StyledCardStyle = styled(Card)`
   background-color: ${props => props.theme.colors.bg.primary};
@@ -11,5 +11,9 @@ export const StyledCardCover = styled(Card.Cover)`
 `;
 
 export const StyledTitle = styled(Title)`
+  font-family: ${props => props.theme.fonts.heading};
+`;
+
+export const StyledParagraph = styled(Paragraph)`
   font-family: ${props => props.theme.fonts.body};
 `;

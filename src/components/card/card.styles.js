4a76b4c40126fed44d1cc950be906f872559e434
Juan <pront23@gmail.com>
2021-06-30 19:01:49 +0900

    added section sectionEnd styledView styledImage

--- src/components/card/card.styles.js
+++ src/components/card/card.styles.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import { Card, Title, Paragraph } from 'react-native-paper';
+import { Image, View, Text } from 'react-native';
 
 export const StyledCardStyle = styled(Card)`
   background-color: ${props => props.theme.colors.bg.primary};
@@ -14,6 +15,36 @@ export const StyledTitle = styled(Title)`
   font-family: ${props => props.theme.fonts.heading};
 `;
 
+export const StyledText = styled(Text)`
+  color: red;
+`;
+
 export const StyledParagraph = styled(Paragraph)`
   font-family: ${props => props.theme.fonts.body};
+`;
+
+export const Rating = styled(View)`
+  flex-direction: row;
+  padding-top: ${props => props.theme.space[2]};
+  padding-bottom: ${props => props.theme.space[2]};
+`;
+
+export const Section = styled(View)`
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const SectionEnd = styled(View)`
+  flex: 1;
+  flex-direction: row;
+  justify-content: flex-end;
+`;
+
+export const StyledView = styled(View)`
+  padding-left: ${props => props.theme.space[2]};
+`;
+
+export const StyledImage = styled(Image)`
+  width: 15px;
+  height: 15px;
 `;

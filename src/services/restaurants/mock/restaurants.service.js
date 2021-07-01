a85b33007a3b4d04279e5b55a8c92e368106c033
Juan <pront23@gmail.com>
2021-07-01 18:37:44 +0900

    added mockImages

--- src/services/restaurants/mock/restaurants.service.js
+++ src/services/restaurants/mock/restaurants.service.js
@@ -1,4 +1,4 @@
-import { mocks } from './index';
+import { mockImages, mocks } from './index';
 import camelize from 'camelize';
 
 export const restaurantsRequest = async (
@@ -14,6 +14,9 @@ export const restaurantsRequest = async (
 
 export const restaurantsTransform = ({ results = [] }) => {
   const mappedResults = results.map(restaurant => {
+    restaurant.photos = restaurant.photos.map(p => {
+      return mockImages[Math.ceil(Math.random() * (mockImages.length - 1))];
+    });
     return {
       ...restaurant,
       isOpenNow: restaurant.opening_hours && restaurant.opening_hours.open_now,
@@ -23,8 +26,3 @@ export const restaurantsTransform = ({ results = [] }) => {
 
   return camelize(mappedResults);
 };
-
-restaurantsRequest()
-  .then(restaurantsTransform)
-  .then(transformedResponse => console.log(transformedResponse))
-  .catch(err => console.log('error', err));

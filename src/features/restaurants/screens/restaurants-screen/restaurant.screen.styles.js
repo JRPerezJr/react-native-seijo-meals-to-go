80f1368a47f8d7159ea1844a182572d931898f1f
Juan <pront23@gmail.com>
2021-07-01 21:29:41 +0900

    created styled loadingContainer

--- src/features/restaurants/screens/restaurants-screen/restaurant.screen.styles.js
+++ src/features/restaurants/screens/restaurants-screen/restaurant.screen.styles.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components/native';
 import { View, FlatList } from 'react-native';
+import { ActivityIndicator, Colors } from 'react-native-paper';
 
 // export const StyledSafeAreaView = styled(SafeAreaView)`
 //   flex: 1;
@@ -15,3 +16,17 @@ export const RestaurantList = styled(FlatList).attrs({
     padding: 16,
   },
 })``;
+
+export const LoadingContainer = styled(View)`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+`;
+
+export const Loading = styled(ActivityIndicator).attrs({
+  size: 'large',
+  animating: true,
+  color: Colors.red800,
+})`
+  margin-left: -25px;
+`;
--- src/features/restaurants/screens/restaurants-screen/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants-screen/restaurants.screen.js
@@ -1,49 +1,48 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import { Searchbar } from 'react-native-paper';
 
 import {
   StyledSearchbarView,
   RestaurantList,
+  Loading,
+  LoadingContainer,
 } from './restaurant.screen.styles';
 
 import { RestaurantInfoCard } from '../../../../components/restaurant-info-card/restaurant-info-card';
 import { Spacer } from '../../../../components/spacer/spacer.component';
 import { StyledSafeAreaView } from '../../../../components/utilities/safe-area.component';
+import { RestaurantsContext } from '../../../../services/restaurants/mock/restaurants.context';
 
 export const RestaurantsScreen = () => {
+  const { isLoading, error, restaurants } = useContext(RestaurantsContext);
+
   return (
     <>
       <StyledSafeAreaView>
         <StyledSearchbarView>
           <Searchbar />
         </StyledSearchbarView>
-        <RestaurantList
-          data={[
-            { name: 1 },
-            { name: 2 },
-            { name: 3 },
-            { name: 4 },
-            { name: 5 },
-            { name: 6 },
-            { name: 7 },
-            { name: 8 },
-            { name: 9 },
-            { name: 10 },
-            { name: 11 },
-            { name: 12 },
-            { name: 13 },
-            { name: 14 },
-          ]}
-          renderItem={() => (
-            <>
-              <Spacer position="bottom" size="large">
-                <RestaurantInfoCard />
-              </Spacer>
-            </>
-          )}
-          keyExtractor={item => item.name}
-        />
+        {isLoading ? (
+          <LoadingContainer>
+            <Loading />
+          </LoadingContainer>
+        ) : (
+          <RestaurantList
+            data={restaurants}
+            renderItem={({ item }) => {
+              console.log(item);
+              return (
+                <>
+                  <Spacer position="bottom" size="large">
+                    <RestaurantInfoCard restaurant={item} />
+                  </Spacer>
+                </>
+              );
+            }}
+            keyExtractor={item => item.name}
+          />
+        )}
       </StyledSafeAreaView>
     </>
   );

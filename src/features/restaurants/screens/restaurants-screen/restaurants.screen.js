3f7b5d486e211d5d5e113e0327871b30057fa5a3
Juan <pront23@gmail.com>
2021-07-03 18:49:25 +0900

    removed incline function component

--- src/features/restaurants/screens/restaurants-screen/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants-screen/restaurants.screen.js
@@ -32,7 +32,11 @@ export const RestaurantsScreen = ({ navigation }) => {
             renderItem={({ item }) => {
               return (
                 <TouchableOpacity
-                  onPress={() => navigation.navigate('RestaurantDetail')}
+                  onPress={() =>
+                    navigation.navigate('RestaurantDetail', {
+                      restaurant: item,
+                    })
+                  }
                 >
                   <Spacer position="bottom" size="large">
                     <RestaurantInfoCard restaurant={item} />

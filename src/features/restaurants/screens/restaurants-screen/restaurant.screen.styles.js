7b93187984ff1b7fae793a71ec57edf3abca34a8
Juan <pront23@gmail.com>
2021-07-03 13:23:44 +0900

    removed StyledSearchBarView

--- src/features/restaurants/screens/restaurants-screen/restaurant.screen.styles.js
+++ src/features/restaurants/screens/restaurants-screen/restaurant.screen.styles.js
@@ -2,15 +2,6 @@ import styled from 'styled-components/native';
 import { View, FlatList } from 'react-native';
 import { ActivityIndicator, Colors } from 'react-native-paper';
 
-// export const StyledSafeAreaView = styled(SafeAreaView)`
-//   flex: 1;
-//   margin-top: ${StatusBar.currentHeight}px;
-// `;
-
-export const StyledSearchbarView = styled(View)`
-  padding: ${props => props.theme.space[3]};
-`;
-
 export const RestaurantList = styled(FlatList).attrs({
   contentContainerStyle: {
     padding: 16,

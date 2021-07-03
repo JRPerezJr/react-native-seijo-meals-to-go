38a040158a42bba51697f91d52a17bc761e8c653
Juan <pront23@gmail.com>
2021-07-03 19:35:58 +0900

    added useEffect

--- src/features/map/components/search/search.component.js
+++ src/features/map/components/search/search.component.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Searchbar } from 'react-native-paper';
 
 import { LocationContext } from '../../../../services/location/location.context';
@@ -9,10 +9,15 @@ export const AppSearchBar = () => {
   const { keyword, search } = useContext(LocationContext);
   const [searchKeyword, setSearchKeyword] = useState(keyword);
 
+  useEffect(() => {
+    setSearchKeyword(keyword);
+  }, [keyword]);
+
   return (
     <StyledSearchbarView>
       <Searchbar
         placeholder="Search for a location"
+        icon="map"
         value={searchKeyword}
         onSubmitEditing={() => {
           search(searchKeyword);

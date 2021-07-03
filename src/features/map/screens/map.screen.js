f081f21fbddf9bc35803653499afef2b96319a09
Juan <pront23@gmail.com>
2021-07-03 19:58:04 +0900

    added map markers

--- src/features/map/screens/map.screen.js
+++ src/features/map/screens/map.screen.js
@@ -1,5 +1,50 @@
-import React from 'react';
-
+import React, { useContext, useEffect, useState } from 'react';
 import MapView from 'react-native-maps';
+import { LocationContext } from '../../../services/location/location.context';
+import { RestaurantsContext } from '../../../services/restaurants/mock/restaurants.context';
+import { AppSearchBar } from '../components/search/search.component';
 
-export const MapScreen = () => <MapView />;
+import { StyledMapView } from './map.screen.styles';
+
+export const MapScreen = () => {
+  const { location } = useContext(LocationContext);
+  const { restaurants = [] } = useContext(RestaurantsContext);
+
+  const [latDelta, setLatDelta] = useState(0);
+
+  const { lat, lng, viewport } = location;
+
+  useEffect(() => {
+    const northeastLat = viewport.northeast.lat;
+    const southwestLat = viewport.southwest.lat;
+
+    setLatDelta(northeastLat - southwestLat);
+  }, [location, viewport]);
+
+  return (
+    <>
+      <AppSearchBar />
+      <StyledMapView
+        region={{
+          latitude: lat,
+          longitude: lng,
+          latitudeDelta: latDelta,
+          longitudeDelta: 0.02,
+        }}
+      >
+        {restaurants.map(restaurant => {
+          return (
+            <MapView.Marker
+              key={restaurant.name}
+              title={restaurant.name}
+              coordinate={{
+                latitude: restaurant.geometry.location.lat,
+                longitude: restaurant.geometry.location.lng,
+              }}
+            />
+          );
+        })}
+      </StyledMapView>
+    </>
+  );
+};

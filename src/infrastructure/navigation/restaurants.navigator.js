a9647dd2e0b53e22d12bde93aee8500ea8db77f9
Juan <pront23@gmail.com>
2021-07-03 18:48:54 +0900

    added restaurant stack navigation

--- src/infrastructure/navigation/restaurants.navigator.js
+++ src/infrastructure/navigation/restaurants.navigator.js
@@ -1,17 +1,30 @@
 import React from 'react';
 
-import { createStackNavigator } from '@react-navigation/stack';
+import {
+  createStackNavigator,
+  TransitionPresets,
+} from '@react-navigation/stack';
 
 import { RestaurantsScreen } from '../../features/restaurants/screens/restaurants-screen/restaurants.screen';
+import { RestaurantDetailScreen } from '../../features/restaurants/screens/restaurant-detail-screen/restaurant-detail.screen';
 
 const RestaurantsStack = createStackNavigator();
 
 export const RestaurantsNavigator = () => {
   return (
-    <RestaurantsStack.Navigator>
+    <RestaurantsStack.Navigator
+      headerMode="none"
+      screenOptions={{
+        ...TransitionPresets.ModalPresentationIOS,
+      }}
+    >
       <RestaurantsStack.Screen
         name="Restaurants"
         component={RestaurantsScreen}
+      />
+      <RestaurantsStack.Screen
+        name="RestaurantDetail"
+        component={RestaurantDetailScreen}
       />
     </RestaurantsStack.Navigator>
   );

5867c95954c8fa33ba37a86ec93822afeb3bddba
Juan <pront23@gmail.com>
2021-07-03 19:23:39 +0900

    corrected import error

--- src/features/settings/screen/settings.screen.js
+++ src/features/settings/screen/settings.screen.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Text } from 'react-native';
-import { StyledSafeAreaView } from '../../../../components/utilities/safe-area.component';
+import { StyledSafeAreaView } from '../../../components/utilities/safe-area.component';
 
 export const SettingsScreen = () => {
   return (

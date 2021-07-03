7d8948f7c999740981823288bec2bc03698d9b77
Juan <pront23@gmail.com>
2021-07-03 13:29:33 +0900

    removed unused imports

--- src/features/restaurants/screens/restaurants-screen/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants-screen/restaurants.screen.js
@@ -1,19 +1,17 @@
 import React, { useContext } from 'react';
 
-import { Searchbar } from 'react-native-paper';
-
 import {
-  StyledSearchbarView,
   RestaurantList,
   Loading,
   LoadingContainer,
 } from './restaurant.screen.styles';
 
-import { RestaurantInfoCard } from '../../../../components/restaurant-info-card/restaurant-info-card';
-import { Spacer } from '../../../../components/spacer/spacer.component';
-import { StyledSafeAreaView } from '../../../../components/utilities/safe-area.component';
 import { RestaurantsContext } from '../../../../services/restaurants/mock/restaurants.context';
+
+import { StyledSafeAreaView } from '../../../../components/utilities/safe-area.component';
 import { AppSearchBar } from '../../components/search/search.component';
+import { Spacer } from '../../../../components/spacer/spacer.component';
+import { RestaurantInfoCard } from '../../../../components/restaurant-info-card/restaurant-info-card';
 
 export const RestaurantsScreen = () => {
   const { isLoading, error, restaurants } = useContext(RestaurantsContext);

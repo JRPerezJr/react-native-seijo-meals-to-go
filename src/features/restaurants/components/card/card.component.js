18a5870670f1eb336665d8f04dce8d4e456bbcd5
Juan <pront23@gmail.com>
2021-07-06 18:57:23 +0900

    moved to new folder location

---
+++ src/features/restaurants/components/card/card.component.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import { Card } from 'react-native-paper';
+
+import {
+  StyledCardCover,
+  StyledCard,
+  StyledTitle,
+  StyledParagraph,
+  Rating,
+  Section,
+  SectionEnd,
+  StyledIcon,
+} from './card.styles';
+
+import { Spacer } from '../../../../components/spacer/spacer.component';
+import { StyledText } from '../../../../components/typography/text.component';
+
+import { Star } from '../../../../../assets/star';
+import { OpenIcon } from '../../../../../assets/open';
+import { Favorite } from '../favorites/favorite.component';
+
+export const CardAppComponent = ({ restaurantData }) => {
+  const {
+    address,
+    icon,
+    isClosedTemporarily,
+    isOpenNow,
+    name,
+    photos,
+    rating,
+    placeId,
+  } = restaurantData.props;
+
+  const ratingArray = Array.from(new Array(Math.floor(rating)));
+
+  return (
+    <StyledCard elevation={5}>
+      <Favorite />
+      <StyledCardCover key={name} source={{ uri: photos[0] }} />
+      <Card.Content>
+        <StyledTitle>{name}</StyledTitle>
+        <Section>
+          <Rating>
+            {ratingArray.map((_, i) => (
+              <Star key={`star-${placeId}-${i}`} />
+            ))}
+          </Rating>
+          <SectionEnd>
+            {isClosedTemporarily && (
+              <StyledText variant="error">CLOSED TEMPORARILY</StyledText>
+            )}
+            <Spacer position="left" size="large">
+              {isOpenNow && <OpenIcon />}
+            </Spacer>
+            <Spacer position="left" size="large">
+              <StyledIcon source={{ uri: icon }} />
+            </Spacer>
+          </SectionEnd>
+        </Section>
+        <StyledParagraph>{address}</StyledParagraph>
+      </Card.Content>
+    </StyledCard>
+  );
+};

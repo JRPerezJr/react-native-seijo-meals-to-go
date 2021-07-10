0247b3c3afce53a1a32f7ac7fcfa8fb50ab77194
Juan <pront23@gmail.com>
2021-07-10 19:28:43 +0900

    removed unused exports

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+const config = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(config);
+}
+
+export default firebase;

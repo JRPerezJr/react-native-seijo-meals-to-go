cadaf3f3bf782b0d266e7fd1d1152aa8d04ab71a
Juan <pront23@gmail.com>
2021-07-10 15:01:23 +0900

    created favorites bar

---
+++ src/components/favorites/favorites-bar/favorites-bar.component.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { ScrollView, Text, TouchableOpacity } from 'react-native';
+
+import { CompactRestaurantInfo } from '../../compact-restaurant-info/compact-restaurant-info.component';
+import { Spacer } from '../../spacer/spacer.component';
+import { FavoritesWrapper } from './favorites-bar.style';
+
+export const FavoritesBar = ({ favorites, onNavigate }) => {
+  if (!favorites.length) {
+    return null;
+  }
+  return (
+    <FavoritesWrapper>
+      <Spacer variant="left.large">
+        <Text variant="caption">Favorites</Text>
+      </Spacer>
+      <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+        {favorites.map(restaurant => {
+          const key = restaurant.name;
+          return (
+            <Spacer key={key} position="left" size="medium">
+              <TouchableOpacity
+                onPress={() =>
+                  onNavigate('RestaurantDetail', {
+                    restaurant,
+                  })
+                }
+              >
+                <CompactRestaurantInfo restaurant={restaurant} />
+              </TouchableOpacity>
+            </Spacer>
+          );
+        })}
+      </ScrollView>
+    </FavoritesWrapper>
+  );
+};
---
+++ src/components/favorites/favorites-bar/favorites-bar.style.js
@@ -0,0 +1,6 @@
+import { View } from 'react-native';
+import styled from 'styled-components';
+
+export const FavoritesWrapper = styled(View)`
+  padding: 10px;
+`;

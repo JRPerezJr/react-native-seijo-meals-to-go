2bb6d1aa1753df889a5c571244546017c8fb8bbd
Juan <pront23@gmail.com>
2021-07-10 15:00:41 +0900

    added favorites functions

--- src/features/restaurants/components/search/search.component.js
+++ src/features/restaurants/components/search/search.component.js
@@ -5,7 +5,7 @@ import { LocationContext } from '../../../../services/location/location.context'
 
 import { StyledSearchbarView } from './search.component.styles';
 
-export const AppSearchBar = () => {
+export const AppSearchBar = ({ isFavoritesToggled, onFavoritesToggle }) => {
   const { keyword, search } = useContext(LocationContext);
   const [searchKeyword, setSearchKeyword] = useState(keyword);
 
@@ -16,6 +16,8 @@ export const AppSearchBar = () => {
   return (
     <StyledSearchbarView>
       <Searchbar
+        icon={isFavoritesToggled ? 'heart' : 'heart-outline'}
+        onIconPress={onFavoritesToggle}
         placeholder="Search for a location"
         value={searchKeyword}
         onSubmitEditing={() => {

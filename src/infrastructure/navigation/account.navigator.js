27697b3b2736885f01a493c7fadb64fc9e029c04
Juan <pront23@gmail.com>
2021-07-10 21:33:27 +0900

    added stack navigations

--- src/infrastructure/navigation/account.navigator.js
+++ src/infrastructure/navigation/account.navigator.js
@@ -1,26 +1,17 @@
 import React from 'react';
+
 import { createStackNavigator } from '@react-navigation/stack';
-import { Text, View } from 'react-native';
+
+import { AccountScreen } from '../../features/account/screens/account-screen/account.screen';
+import { LoginScreen } from '../../features/account/screens/login-screen/login.screen';
+import { RegisterScreen } from '../../features/account/screens/register/register.screen';
 
 const Stack = createStackNavigator();
 
 export const AccountNavigator = () => (
   <Stack.Navigator headerMode="none">
-    <Stack.Screen
-      name="Main"
-      component={() => (
-        <View>
-          <Text>Account Screen</Text>
-        </View>
-      )}
-    />
-    <Stack.Screen
-      name="Login"
-      component={() => (
-        <View>
-          <Text>Login Screen</Text>
-        </View>
-      )}
-    />
+    <Stack.Screen name="Main" component={AccountScreen} />
+    <Stack.Screen name="Login" component={LoginScreen} />
+    <Stack.Screen name="Register" component={RegisterScreen} />
   </Stack.Navigator>
 );

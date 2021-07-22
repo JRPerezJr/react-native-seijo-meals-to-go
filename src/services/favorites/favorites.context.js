d6c75499ec5535afd68e2fb66b6c931d868ed173
Juan <pront23@gmail.com>
2021-07-22 20:09:47 +0900

    added user context to track favorites

--- src/services/favorites/favorites.context.js
+++ src/services/favorites/favorites.context.js
@@ -1,23 +1,26 @@
-import React, { useState, createContext, useEffect } from 'react';
+import React, { useState, createContext, useEffect, useContext } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { AuthenticationContext } from '../authentication/authentication.context';
 
 export const FavoritesContext = createContext();
 
 export const FavoritesContextProvider = ({ children }) => {
+  const { user } = useContext(AuthenticationContext);
+
   const [favorites, setFavorites] = useState([]);
 
-  const saveFavorites = async value => {
+  const saveFavorites = async (value, uid) => {
     try {
       const jsonValue = JSON.stringify(value);
-      await AsyncStorage.setItem('@favorites', jsonValue);
+      await AsyncStorage.setItem(`@favorites${uid}`, jsonValue);
     } catch (e) {
       console.log('There was an error saving favorites');
     }
   };
 
-  const loadFavorites = async () => {
+  const loadFavorites = async uid => {
     try {
-      const jsonValue = await AsyncStorage.getItem('@favorites');
+      const jsonValue = await AsyncStorage.getItem(`@favorites${uid}`);
       return jsonValue != null ? setFavorites(JSON.parse(jsonValue)) : null;
     } catch (e) {
       console.log('There was an error fetching favorites');
@@ -37,12 +40,16 @@ export const FavoritesContextProvider = ({ children }) => {
   };
 
   useEffect(() => {
-    loadFavorites();
-  }, []);
+    if (user) {
+      loadFavorites(user.uid);
+    }
+  }, [user]);
 
   useEffect(() => {
-    saveFavorites(favorites);
-  }, [favorites]);
+    if (user) {
+      saveFavorites(favorites, user.uid);
+    }
+  }, [favorites, user]);
 
   return (
     <FavoritesContext.Provider

68c90bf77c704d82fb847b36b2a60a8b75b3c416
Juan <pront23@gmail.com>
2021-07-22 19:58:52 +0900

    added logout button

--- src/features/settings/screen/settings.screen.js
+++ src/features/settings/screen/settings.screen.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useContext } from 'react';
+import { Button, Text } from 'react-native';
 import { StyledSafeAreaView } from '../../../components/utilities/safe-area.component';
+import { AuthenticationContext } from '../../../services/authentication/authentication.context';
 
 export const SettingsScreen = () => {
+  const { onLogout } = useContext(AuthenticationContext);
+
   return (
     <>
       <StyledSafeAreaView>
         <Text>Hello from Settings</Text>
+        <Button title="logout" onPress={() => onLogout()} />
       </StyledSafeAreaView>
     </>
   );

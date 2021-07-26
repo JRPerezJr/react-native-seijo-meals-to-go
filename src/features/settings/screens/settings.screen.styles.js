585c6052993b077147094e269e4770d06da9fefb
Juan <pront23@gmail.com>
2021-07-26 09:27:29 +0900

    added avatar container

--- src/features/settings/screens/settings.screen.styles.js
+++ src/features/settings/screens/settings.screen.styles.js
@@ -1,6 +1,11 @@
+import { View } from 'react-native';
 import { List } from 'react-native-paper';
 import styled from 'styled-components';
 
-export const SettingsList = styled(List.Item).attrs({
-  style: { padding: 16 },
-})``;
+export const SettingsList = styled(List.Item)`
+  padding: ${props => props.theme.space[3]};
+`;
+
+export const AvatarContainer = styled(View)`
+  align-items: center;
+`;

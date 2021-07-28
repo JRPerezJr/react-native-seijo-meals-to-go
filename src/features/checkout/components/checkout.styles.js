04c55b706eb5debf8123519959006b04cad3c7af
Juan <pront23@gmail.com>
2021-07-29 00:49:57 +0900

    added form input styles

--- src/features/checkout/components/checkout.styles.js
+++ src/features/checkout/components/checkout.styles.js
@@ -1,7 +1,8 @@
 import styled from 'styled-components/native';
 
 import { View } from 'react-native';
-import { Avatar } from 'react-native-paper';
+import { Avatar, TextInput, Button } from 'react-native-paper';
+import { colors } from '../../../infrastructure/theme/colors';
 
 export const CartIconContainer = styled(View)`
   align-items: center;
@@ -14,4 +15,66 @@ export const CartIcon = styled(Avatar.Icon).attrs({
   icon: 'cart-off',
 })`
   background-color: ${props => props.bg || props.theme.colors.brand.primary};
+`;
+
+export const NameInput = styled(TextInput).attrs({
+  label: 'Full Name',
+})`
+  margin: ${props => props.theme.space[3]};
+`;
+
+export const AddressInput1 = styled(TextInput).attrs({
+  label: 'Address1',
+})`
+  margin: ${props => props.theme.space[3]};
+`;
+
+export const AddressInput2 = styled(TextInput).attrs({
+  label: 'Address2',
+})`
+  margin: ${props => props.theme.space[3]};
+`;
+
+export const CityInput = styled(TextInput).attrs({
+  label: 'City',
+})`
+  margin: ${props => props.theme.space[3]};
+`;
+
+export const StateInput = styled(TextInput).attrs({
+  label: 'State',
+})`
+  margin: ${props => props.theme.space[3]};
+`;
+
+export const CountryInput = styled(TextInput).attrs({
+  label: 'Country',
+})`
+  margin: ${props => props.theme.space[3]};
+`;
+
+export const ZipCodeInput = styled(TextInput).attrs({
+  label: 'ZipCode',
+})`
+  margin: ${props => props.theme.space[3]};
+`;
+
+export const PayButton = styled(Button).attrs({
+  icon: 'cash-usd',
+  mode: 'contained',
+  color: colors.brand.primary,
+})`
+  width: 80%;
+  align-self: center;
+  padding: ${props => props.theme.space[2]};
+`;
+
+export const ClearButton = styled(Button).attrs({
+  icon: 'cart-off',
+  mode: 'contained',
+  color: colors.ui.error,
+})`
+  width: 80%;
+  align-self: center;
+  padding: ${props => props.theme.space[2]};
 `;

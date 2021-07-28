6fcad23bacc17fff95054b7e9af64dec4db32261
Juan <pront23@gmail.com>
2021-07-28 13:57:56 +0900

    added order button component

---
+++ src/features/restaurants/components/order-button/order-button.styles.js
@@ -0,0 +1,11 @@
+import { Button } from 'react-native-paper';
+import styled from 'styled-components/native';
+import { colors } from '../../../../infrastructure/theme/colors';
+
+export const OrderButton = styled(Button).attrs({
+  color: colors.brand.primary,
+})`
+  padding: ${props => props.theme.space[2]};
+  width: 80%;
+  align-self: center;
+`;

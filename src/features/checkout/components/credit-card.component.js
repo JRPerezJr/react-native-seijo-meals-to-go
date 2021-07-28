b1f48a43365204c1b6252558bb503002cc24e975
Juan <pront23@gmail.com>
2021-07-29 00:50:55 +0900

    added inputs for address city state zipcode country

--- src/features/checkout/components/credit-card.component.js
+++ src/features/checkout/components/credit-card.component.js
@@ -3,7 +3,15 @@ import React from 'react';
 import { LiteCreditCardInput } from 'react-native-credit-card-input';
 import { cardTokenRequest } from '../../../services/checkout/checkout.service';
 
-export const CreditCardInput = ({ name = 'JRod' }) => {
+export const CreditCardInput = ({
+  name,
+  address1,
+  address2,
+  city,
+  state,
+  zipCode,
+  country,
+}) => {
   const onChange = async formData => {
     const { values, status } = formData;
     const isIncomplete = Object.values(status).includes('incomplete');
@@ -15,6 +23,12 @@ export const CreditCardInput = ({ name = 'JRod' }) => {
       exp_year: expiry[1],
       cvc: values.cvc,
       name,
+      address_line1: address1,
+      address_line2: address2,
+      address_city: city,
+      address_state: state,
+      address_zip: zipCode,
+      address_country: country,
     };
     const info = await cardTokenRequest(card);
     console.log(info);

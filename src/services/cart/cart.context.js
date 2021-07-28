e33fe3e1ee7272fb0aac5a788f28919fa8fb68cb
Juan <pront23@gmail.com>
2021-07-28 13:18:56 +0900

    added cart context

---
+++ src/services/cart/cart.context.js
@@ -0,0 +1,37 @@
+import React, { createContext, useState, useEffect, useContext } from 'react';
+
+import { AuthenticationContext } from '../authentication/authentication.context';
+
+export const CartContext = createContext();
+
+export const CartContextProvider = ({ children }) => {
+  const { user } = useContext(AuthenticationContext);
+
+  const [cart, setCart] = useState([]);
+  const [restaurant, setRestaurant] = useState(null);
+
+  const add = (item, rest) => {
+    if (!restaurant || restaurant.placeId !== rest.placeId) {
+      setRestaurant(rest);
+      setCart([item]);
+    }
+    setCart([...cart, item]);
+  };
+
+  const clearCart = () => {
+    setCart([]);
+    setRestaurant(null);
+  };
+  return (
+    <CartContext.Provider
+      values={{
+        addToCart: add,
+        clearCart: clearCart,
+        restaurant,
+        cart,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+};

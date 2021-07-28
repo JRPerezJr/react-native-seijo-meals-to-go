adecc3ad6d8ec79819ffe8c11f0bbf5617f51bcd
Juan <pront23@gmail.com>
2021-07-28 14:12:01 +0900

    added icon color and mode attributes

--- src/features/restaurants/components/order-button/order-button.styles.js
+++ src/features/restaurants/components/order-button/order-button.styles.js
@@ -4,6 +4,8 @@ import { colors } from '../../../../infrastructure/theme/colors';
 
 export const OrderButton = styled(Button).attrs({
   color: colors.brand.primary,
+  icon: 'cash-usd',
+  mode: 'contained',
 })`
   padding: ${props => props.theme.space[2]};
   width: 80%;

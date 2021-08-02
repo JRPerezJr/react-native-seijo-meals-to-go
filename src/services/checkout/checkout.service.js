5c099c0a2b7a6ac77578b64b904ff50a99669e2b
Juan <pront23@gmail.com>
2021-08-02 22:07:09 +0900

    fixed error screen bug

--- src/services/checkout/checkout.service.js
+++ src/services/checkout/checkout.service.js
@@ -6,14 +6,15 @@ const API_URL = 'http://localhost:8000/v1';
 
 export const cardTokenRequest = card => stripe.createToken({ card });
 
-export const payRequest = async (token, amount) => {
-  try {
-    return await fetch(`${API_URL}/payments`, {
-      method: 'post',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ token, amount }),
-    });
-  } catch (error) {
-    return console.log('There was an issue with your payment.');
-  }
+export const payRequest = (token, amount) => {
+  return fetch(`${API_URL}/payments`, {
+    method: 'post',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ token, amount }),
+  }).then(res => {
+    if (res.status > 200) {
+      return Promise.reject('Something went wrong processing your payment');
+    }
+    return res.json();
+  });
 };

4866036e9afd267be0c7234b0e68f878eab4f24b
Juan <pront23@gmail.com>
2021-08-02 18:47:46 +0900

    added PaymentProcessing component

--- src/features/checkout/components/checkout.styles.js
+++ src/features/checkout/components/checkout.styles.js
@@ -1,13 +1,30 @@
 import styled from 'styled-components/native';
 
 import { View } from 'react-native';
-import { Avatar, TextInput, Button } from 'react-native-paper';
+import {
+  ActivityIndicator,
+  Avatar,
+  TextInput,
+  Button,
+  Colors,
+} from 'react-native-paper';
 import { colors } from '../../../infrastructure/theme/colors';
 
 export const CartIconContainer = styled(View)`
   align-items: center;
   justify-content: center;
   flex: 1;
+`;
+
+export const PaymentProcessing = styled(ActivityIndicator).attrs({
+  size: 128,
+  animating: true,
+  color: Colors.blue300,
+})`
+  position: absolute;
+  top: 50%;
+  left: 35%;
+  z-index: 999;
 `;
 
 export const CartIcon = styled(Avatar.Icon).attrs({

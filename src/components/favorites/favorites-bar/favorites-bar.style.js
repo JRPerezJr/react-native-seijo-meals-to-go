f1cdbd835cab6bdfcdb1393098312cb4b1f7b92b
Juan <pront23@gmail.com>
2021-08-03 18:48:01 +0900

    round card and adjusted elevation

--- src/components/favorites/favorites-bar/favorites-bar.style.js
+++ src/components/favorites/favorites-bar/favorites-bar.style.js
@@ -1,6 +1,10 @@
-import { View } from 'react-native';
+import { Card } from 'react-native-paper';
 import styled from 'styled-components';
 
-export const FavoritesWrapper = styled(View)`
+export const FavoritesWrapper = styled(Card).attrs({
+  elevation: 3,
+})`
   padding: 10px;
+  z-index: 999;
+  border-radius: 15px;
 `;

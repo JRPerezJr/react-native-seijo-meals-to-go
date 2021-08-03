1106642135dda9cd6e8acae03518b8d9bad15f80
Juan <pront23@gmail.com>
2021-08-03 18:33:18 +0900

    added dividers

--- src/features/restaurants/screens/restaurant-detail-screen/restaurant-detail.screen.js
+++ src/features/restaurants/screens/restaurant-detail-screen/restaurant-detail.screen.js
@@ -2,10 +2,11 @@ import React, { useState, useContext } from 'react';
 
 import { ScrollView } from 'react-native';
 
-import { List } from 'react-native-paper';
+import { Divider, List } from 'react-native-paper';
 
 import { Spacer } from '../../../../components/spacer/spacer.component';
 import { StyledSafeAreaView } from '../../../../components/utilities/safe-area.component';
+import { colors } from '../../../../infrastructure/theme/colors';
 import { CartContext } from '../../../../services/cart/cart.context';
 import { OrderButton } from '../../components/order-button/order-button.styles';
 import { RestaurantInfoCard } from '../../components/restaurant-info-card/restaurant-info-card';
@@ -31,9 +32,13 @@ export const RestaurantDetailScreen = ({ navigation, route }) => {
           onPress={() => setBreakfastExpanded(!breakfastExpanded)}
         >
           <List.Item title="Eggs Benedict" />
+          <Divider />
           <List.Item title="Breakfast Sandwich" />
+          <Divider />
           <List.Item title="Breakfast Burrito" />
+          <Divider />
         </List.Accordion>
+        <Divider />
         <List.Accordion
           title="Lunch"
           left={props => <List.Icon {...props} icon="hamburger" />}
@@ -41,9 +46,13 @@ export const RestaurantDetailScreen = ({ navigation, route }) => {
           onPress={() => setLunchExpanded(!lunchExpanded)}
         >
           <List.Item title="Burger and Fries" />
+          <Divider />
           <List.Item title="Fish and Chips" />
+          <Divider />
           <List.Item title="Chicken Sandwich" />
+          <Divider />
         </List.Accordion>
+        <Divider />
         <List.Accordion
           title="Dinner"
           left={props => <List.Icon {...props} icon="food-variant" />}
@@ -51,9 +60,13 @@ export const RestaurantDetailScreen = ({ navigation, route }) => {
           onPress={() => setDinnerExpanded(!dinnerExpanded)}
         >
           <List.Item title="Angus Steak" />
+          <Divider />
           <List.Item title="Chicken Curry Rice" />
+          <Divider />
           <List.Item title="Smoked Salmon" />
+          <Divider />
         </List.Accordion>
+        <Divider />
         <List.Accordion
           title="Drinks"
           left={props => <List.Icon {...props} icon="cup" />}
@@ -61,8 +74,11 @@ export const RestaurantDetailScreen = ({ navigation, route }) => {
           onPress={() => setDrinksExpanded(!drinksExpanded)}
         >
           <List.Item title="Coke" />
+          <Divider />
           <List.Item title="Vodka" />
+          <Divider />
           <List.Item title="Sake" />
+          <Divider />
         </List.Accordion>
       </ScrollView>
       <Spacer position="bottom" size="large">
